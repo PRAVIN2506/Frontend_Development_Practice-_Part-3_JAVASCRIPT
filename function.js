67b47fb597b400f0650885a9cd31bbb035b13929
PRAVIN B <peterpravin2506@gmail.com>
2024-09-05 22:19:35 +0530

    added some functions

--- function.js
+++ function.js
@@ -9,6 +9,7 @@ function_name();
 function_name();
 function_name();
 
+
 // 2.function expression (storing function inside a variable name is knowwn as funtion expression)
 
 var sayhi= function f_name(parameters){
@@ -41,7 +42,7 @@ sayhi();
               //example for hoisting-->>
               
               console.log(add(10,30));  //due to hoisting it is possible
-              console.log(sub(10,30));  //it is not possible bcoz hoisting for function expression 
+              // console.log(sub(10,30));  //it is not possible bcoz hoisting for function expression 
               
               
               function add(a,b){
@@ -51,3 +52,54 @@ sayhi();
               var sub=function s(a,b){
                 return a-b;
               }
+
+
+// 3.anonymous function (function without function name is known as anonymous function)
+
+var sum= function(){
+  console.log("anonymous function")
+  
+}
+
+sum();
+
+
+// 4.first class function (or) first citizen function (the function stored inside the varbiale is known as first class function)
+
+var sum= function add(a,b) {
+  return a+b;
+}
+
+console.log(sum(10,10));
+
+// 5.IIFE(Imemdiate Invoking Function Expression)
+//    *it can use only one time in the source
+
+// 1st way to create IIFE 
+var div=(function divide(a,b){
+  return a/b;
+})(10,2)
+
+console.log(div);
+
+// 2nd way to create IIFE 
+var div=(function divide(a,b){
+  return a/b;
+}(15,2))
+
+console.log(div);
+
+// 6.Higher Order Function (the function which having another function as an argument is known as HOF)
+
+// 7.callback function (the function which is passed as an argument for an HOF is known as callback function)
+
+function add(a,b){
+  return a+b;
+}
+
+function operation(c,d,callback){
+  return callback(c,d);
+}
+
+console.log(operation(10,5,add));
+
