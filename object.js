07bd733796e285900ac0ce9470259cf4ae0d56d8
PRAVIN B <peterpravin2506@gmail.com>
2024-12-20 22:37:50 +0530

    object

---
+++ object.js
@@ -0,0 +1,17 @@
+let person={
+  name:"pravin",
+  age:20,
+  gender:"male",
+  sibling:{
+    brother1:"jagan",
+    brother2:"raghul"
+  }
+}
+
+//dot notation
+console.log(person.age)
+
+console.log(person.sibling.brother1)
+
+//bracket notation
+console.log(person['name'])
